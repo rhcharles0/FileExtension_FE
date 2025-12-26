269421ea6680f56de830b9bb219da7a28d40e411
Charles <rhcharles0@gmail.com>
2025-12-27 07:44:47 +0900

    feat: 파일 확장자 추가 페이지

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import CheckSection from '@/components/file-extension/check-section';
+import InputSection from '@/components/file-extension/input-section';
+
+const Page = () => {
+    return (
+        <div className="flex justify-center w-full h-full">
+            <div className="mt-10 p-5 w-120 border-gray-500 rounded-2xl border-2">
+                <CheckSection />
+                <InputSection />
+            </div>
+        </div>
+    );
+};
+
+export default Page;
