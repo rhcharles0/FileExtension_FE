3d8dd9b0a5e320ccc3c52dbb4394a1a00d19882b
Charles <rhcharles0@gmail.com>
2025-12-27 07:44:03 +0900

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to the File Extension Manager</h1>
+        </div>
+    );
+}
